355251645b2480f707e5c1c2861c508c516b5e75
Leonardo <troncosoortizleonard@gmail.com>
2019-07-13 11:28:05 -0500

    Clase Datos terminadas

---
+++ app/src/main/java/com/example/personasmaterial/Datos.java
@@ -0,0 +1,40 @@
+package com.example.personasmaterial;
+
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+
+import java.util.ArrayList;
+
+public class Datos {
+    private static String db ="Personas";
+
+    private static DatabaseReference databaseReference =
+    FirebaseDatabase.getInstance().getReference();
+    public static ArrayList<Persona> personas = new ArrayList<>();
+
+    public static void agregar(Persona p) {
+        databaseReference.child(db).child(p.getId()).setValue(p);
+
+
+    }
+
+    public static void editar(Persona p) {
+        databaseReference.child(db).child(p.getId()).setValue(p);
+    }
+
+    public static void eliminar(Persona p) {
+        databaseReference.child(db).child(p.getId()).removeValue();
+    }
+    // manda una peticion al servidor y pide una llave unica
+    public static String getDb() {
+        return databaseReference.push().getKey();
+    }
+
+    public static void setPersonas(ArrayList<Persona> personas){
+        Datos.personas=personas;
+    }
+
+    public  static ArrayList<Persona> obtener() {
+        return Datos.personas;
+    }
+}
---
+++ app/src/main/java/com/example/personasmaterial/Persona.java
@@ -0,0 +1,51 @@
+package com.example.personasmaterial;
+
+public class Persona {
+    private String id;
+    private int foto;
+    private String nombre;
+    private String apellido;
+
+    public Persona(){
+
+    }
+
+    public Persona(String id, int foto, String nombre, String apellido) {
+        this.id = id;
+        this.foto = foto;
+        this.nombre = nombre;
+        this.apellido = apellido;
+    }
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public int getFoto() {
+        return foto;
+    }
+
+    public void setFoto(int foto) {
+        this.foto = foto;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public String getApellido() {
+        return apellido;
+    }
+
+    public void setApellido(String apellido) {
+        this.apellido = apellido;
+    }
+}
